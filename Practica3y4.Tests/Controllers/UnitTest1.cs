e4ef276f1041e1de91e314a6fa595c5543ee1ac8
LudwinGit <ludwinburrion@gmail.com>
2019-10-15 12:22:06 -0600

    Agregando librerias

---
+++ Practica3y4.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practica3y4.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int uno = 1;
+
+            Assert.IsTrue(uno == 1);
+        }
+    }
+}
