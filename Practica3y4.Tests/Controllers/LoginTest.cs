d7cab41b7f9e466b0c76820cd8a6abc0a2062508
huriel2g <hurieluv2g2d93@gmail.com>
2019-10-15 20:45:03 -0600

    Add Pruebas para el Login - 201403841

---
+++ Practica3y4.Tests/Controllers/LoginTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica3y4.Controllers;
+
+namespace Practica3y4.Tests.Controllers
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void validarCadenaPassword() //Prueba que el password contenga caracteres permitidos
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.validarCadenaPassword() ;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void validarLongitudPassword() //Prueba que la longitud del password ente entre [5,8] caracteres
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.validarLongitudPassword();
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void validarCadenaCuenta()   //Prueba que el string de la cuenta posea solo numeros
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.validarCadenaCuenta();
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void validarLongitudCuenta() //Prueba que la longitud de la cadena de la cuenta sea de 6 caracteres
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.validarLongitudCuenta();
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void validarExistenciaCuenta()   //Prueba si la cuenta existe en la base de datos
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.validarExistenciaCuenta();
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void pruebaLogueoCorrecto()  
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.Loguear("123456","correcto");
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void pruebaLogueoCuentaIncorrecta()
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.Loguear("123@23", "password");
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        public void pruebaLogueoPasswordIncorrecta()
+        {
+            LoginController controller = new LoginController();
+            // Act
+            bool result = controller.Loguear("12345", "...@...");
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Practica3y4/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica3y4.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public bool validarCadenaPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validarCadenaCuenta()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validarExistenciaCuenta()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validarLongitudPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool validarLongitudCuenta()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Loguear(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
