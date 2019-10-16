0d5ce051121d06d0d9de252a89a3d92534b62cff
Ludwin <LudwinBurrion@gmail.com>
2019-10-15 21:51:15 -0600

    Agregando Prueba unitaria Usuario

---
+++ Practica3y4.Tests/Controllers/UsuarioTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Practica3y4.Tests.Controllers
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
